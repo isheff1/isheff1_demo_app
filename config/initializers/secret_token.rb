bb41efd34965fa71947a4168b26170a6cd64aec4
Ian Shefferman <isheff1@umbc.edu>
2014-02-06 16:58:33 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Isheff1DemoApp::Application.config.secret_key_base = '9d57ca3749eefe40dde996e56154e72153df432aac48de453620a35771dd3b6898ffe2871f3708e6675e442c89abef6cf2756fd94fc53e632fe2a15bdbfca232'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Isheff1DemoApp::Application.config.session_store :cookie_store, key: '_isheff1_demo_app_session'
